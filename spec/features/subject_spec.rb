fa2f69c511d67fb04ed4ed2deb393004c333d95b
Prakash <plingaiah@qwinixtech.com>
2015-01-20 15:18:53 +0530

    Added css for home page.

--- spec/features/subject_spec.rb
+++ spec/features/subject_spec.rb
@@ -5,7 +5,7 @@ feature "visiting home page" do
 	before(:each) { visit "/" }
 
 	scenario "User should see the title" do 
-		expect(page).to have_text("welcome to Q&A")
+		expect(page).to have_text("Welcome to Q&A")
 	end
 
 	scenario "User should see the subject image links" do 
@@ -13,5 +13,7 @@ feature "visiting home page" do
 		expect(page).to have_xpath("//a/img[@alt='Rails' and @src = '/assets/rails.jpg']")
 		expect(page).to have_xpath("//a/img[@alt='Jquery' and @src = '/assets/jquery.jpeg']")
 		expect(page).to have_xpath("//a/img[@alt='Html css' and @src = '/assets/html_css.jpg']")
+		expect(page).to have_xpath("//a/img[@alt='Testing' and @src = '/assets/testing.jpg']")
+		expect(page).to have_xpath("//a/img[@alt='Devops' and @src = '/assets/devops.jpg']")
 	end
 end

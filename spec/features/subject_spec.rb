e88bf9886a60c7a923fc4e7e4877610c2536b4f4
Prakash <plingaiah@qwinixtech.com>
2015-01-23 12:31:49 +0530

    Added back button feature and also modified existing spec for checking URL.

--- spec/features/subject_spec.rb
+++ spec/features/subject_spec.rb
@@ -22,61 +22,79 @@ feature "checking text in questions page" do
     before(:each) { visit "/" }
     
     scenario "User should see the title in ruby questions page" do
-    	click_link "ruby_redirection"
+    	click_link "ruby_redirection", href: '/questions/ruby'
         expect(page).to have_text("Ruby Questions")
     end
 
     scenario "User should see the title in rails questions page" do
-    	click_link "rails_redirection"
+    	click_link "rails_redirection", href: '/questions/rails'
         expect(page).to have_text("Rails Questions")
     end
 
     scenario "User should see the title in html questions page" do
-    	click_link "html_redirection"
-        expect(page).to have_text("Html and Css Questions")
+    	click_link "html_redirection",href: '/questions/html&css'
+        expect(page).to have_text("Html&Css Questions")
     end
 
     scenario "User should see the title in jquery questions page" do
-    	click_link "jquery_redirection"
-        expect(page).to have_text("Javascript and Jquery Questions")
+    	click_link "jquery_redirection", href: '/questions/javascript&jquery'
+        expect(page).to have_text("Javascript&Jquery Questions")
     end
 
     scenario "User should see the title in devops questions page" do
-    	click_link "devops_redirection"
+    	click_link "devops_redirection", href: '/questions/devops'
         expect(page).to have_text("Devops Questions")
     end
 
     scenario "User should see the title in testing questions page" do
-    	click_link "testing_redirection"
+    	click_link "testing_redirection", href: '/questions/testing'
         expect(page).to have_text("Testing Questions")
     end
 end
 
-feature "checking ask question button in question page" do
+feature "checking ask question link in question page" do
     before(:each) { visit "/" }
     after(:each) {expect(page).to have_button("Ask Question")}
 
-   scenario "User should see the ask question button in ruby questions page" do
+   scenario "User should see the ask question link in ruby questions page" do
         click_link "ruby_redirection"
     end
 
-     scenario "User should see the ask question button in rails questions page" do
+     scenario "User should see the ask question link in rails questions page" do
         click_link "rails_redirection"
     end
 
-    scenario "User should see the ask question button in html questions page" do
+    scenario "User should see the ask question link in html questions page" do
         click_link "html_redirection"
     end
 
-    scenario "User should see the ask question button in jquery questions page" do
+    scenario "User should see the ask question link in jquery questions page" do
         click_link "jquery_redirection"
     end
 
-    scenario "User should see the ask question button in devops questions page" do
+    scenario "User should see the ask question link in devops questions page" do
         click_link "devops_redirection"
     end
 
-    scenario "User should see the ask question button in testing questions page" do
+    scenario "User should see the ask question link in testing questions page" do
         click_link "testing_redirection"
     end
 end
+ 
+feature "Go back to the home page" do
+   
+   scenario "user should see text 'Welcome to Q&A' when come back fron questions page" do
+      visit "questions/:catagory"
+      click_button "Back"
+      expect(page).to have_text("Welcome to Q&A")
+  end
+end
+
+# feature "Titalize the catagory" do
+
+#     scenario "user should see text 'Ruby Questions' when follow ruby image link" do
+#         visit "/"
+#         click_link "ruby_redirection", href: '/questions/ruby'
+#         expect(page).to have_text("Ruby Questions")
+#     end
+# end

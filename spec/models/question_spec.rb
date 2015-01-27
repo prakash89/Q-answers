f529808bea43096734a591407fe982fe2218c497
Prakash <plingaiah@qwinixtech.com>
2015-01-27 17:13:12 +0530

    Added validation for title and description and also wrote test cases for it.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation of title and description fields" do
+
+  	before(:each) do
+  		@q = Question.new
+  	end
+
+  	it "user should validate presence title and description positive" do
+  		@q.title = "some text"
+  		@q.description = "some more text"
+  		expect(@q).to be_valid
+  	end
+
+  	it "user should validate presence title and description negative" do
+  		@q.title = "some text"
+  		@q.description = ""
+  		expect(@q).to be_invalid
+  	end
+
+  	it "user should validate presence title and description negative" do
+  		@q.title = ""
+  		@q.description = "some more text"
+  		expect(@q).to be_invalid
+  	end
+  end
 end

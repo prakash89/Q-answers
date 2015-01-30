45e6bb278677f65f6d1590d8c3edb175ffcae81e
Prakash <plingaiah@qwinixtech.com>
2015-01-30 12:52:43 +0530

    Refacterd the code.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,22 +1,26 @@
 class AnswersController < ApplicationController
+
+    before_action :category_params
 	
 	def new
-		@category = params[:category]
         @question_id = params[:question_id]
         @answer = Answer.new
     end
     
     def create
-        @category = params[:category]
         @question_id = params[:question_id]
-    	@answer = Answer.new(answer_params)
+        @answer = Answer.new(answer_params)
         @answer.question_id = @question_id
         @answer.category = @category
-    	if @answer.save
-    		redirect_to questions_show_path(@category, @question_id)
-    	else
-    		render 'new'
-    	end
+        if @answer.save
+            redirect_to questions_show_path(@category, @question_id)
+        else
+            render 'new'
+        end
+    end
+
+    def category_params
+        @category = params[:category]
     end
 
     private
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,16 @@
 class QuestionsController < ApplicationController
 
+  before_action :category_params
+
   def index
-    @category = params[:category]
     @questions = Question.all.reverse
   end
 
   def new
-    @category = params[:category]
     @question = Question.new
   end
 
   def create
-  	@category = params[:category]
   	@question = Question.new(questions_params)
   	@question.category = @category
   	if @question.valid? && @question.save
@@ -22,24 +21,25 @@ class QuestionsController < ApplicationController
   end
 
   def show
-  	@category = params[:category]
   	@question = Question.find(params[:id])
     @answers = Answer.where("question_id = ?", @question.id)
   end
 
   def edit
-    @category = params[:category]
     @question = Question.find(params[:id])
   end
 
   def update
-    @category = params[:category]
     @question = Question.find(params[:id])
     if @question.update(questions_params)
       redirect_to questions_index_path
     else
       render 'edit'
     end
+  end
+
+  def category_params
+    @category = params[:category]
   end
 
   private

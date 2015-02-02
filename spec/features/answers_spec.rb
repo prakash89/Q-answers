f521cc82830476cf90ef17dd437ac1ba9b9da1e3
Prakash <plingaiah@qwinixtech.com>
2015-02-02 18:29:19 +0530

    wrote feture test case for edit answer

--- spec/features/answers_spec.rb
+++ spec/features/answers_spec.rb
@@ -21,4 +21,23 @@ before(:each) { apllication_signin }
 		click_button("Post Your Answer")
 		expect(page).to have_text("My Answer")
 	end
+
+	scenario "user should edit answer in edit answer page" do
+		question
+    answer
+    visit "/questions/ruby"
+    click_link "some text"
+    expect(page).to have_link("Share Your Answer")
+		click_link("Share Your Answer")
+		expect(page).to have_text("Post Answer Here")
+		expect(page).to have_text("My answer")
+		fill_in "My answer", :with => "My Answer"
+		click_button("Post Your Answer")
+		expect(page).to have_link("Edit Answer")
+		click_link("Edit Answer")
+		expect(page).to have_text("My answer")
+		fill_in "My answer", :with => "My New Answer"
+		click_button("Update Your Answer")
+		expect(page).to have_text("My New Answer")
+  end
 end

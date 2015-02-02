eb22e2d4a0f0acaaf3f6d130d35b2fd58d043183
Prakash <plingaiah@qwinixtech.com>
2015-02-02 10:18:09 +0530

    Added devise gem and did authentication part.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+
+  devise_for :users
   get 'subjects/index'
   root :to => 'subjects#index'
 
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    email "example@yopmail.com"
+    password "12345678"
+  end
+
+end

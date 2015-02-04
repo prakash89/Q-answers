93f1706e95e9c4e28cb0154262e741d6c00fd374
Prakash <plingaiah@qwinixtech.com>
2015-02-04 17:04:22 +0530

    some styles and model test case for answer.

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,6 +1,6 @@
 # require 'rails_helper'
 
-# RSpec.describe SubjectsController, :type => :controller do
+# RSpec.describe CategoriesController, :type => :controller do
 
 #   describe "GET index" do
 #     it "returns http success" do
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Answer, :type => :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation of my answer" do
+
+    before(:each) do
+      @a = Answer.new
+    end
+
+    it "user should validate presence my answer positive" do
+      @a.my_answer = "My Answer"
+      expect(@a).to be_valid
+    end
+
+    it "user should validate presence my answer negative" do
+      @a.my_answer = ""
+      expect(@a).to be_invalid
+    end
+  end  
 end

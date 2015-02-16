239e25b355789af6a2c8bdca27f6720dd5530401
Prakash <plingaiah@qwinixtech.com>
2015-02-16 09:53:48 +0530

    rspec model test case for search is done.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -26,4 +26,17 @@ RSpec.describe Question, :type => :model do
   		expect(@q).to be_invalid
   	end
   end
+
+  describe "search", :search => true do
+    before(:each) do
+      @q = Question.all
+    end
+
+    it "user should search exist question in category show page" do
+      question1 = FactoryGirl.create(:question, title: "first question")
+      question2 = FactoryGirl.create(:question, title: "second question")
+      question3 = FactoryGirl.create(:question, title: "third question")
+      @q.search("f").should == [question1]
+    end
+  end
 end

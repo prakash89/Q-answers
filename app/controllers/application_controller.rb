4568ff4d74faf227d1fe53c86dffab85d07cee6b
Prakash <plingaiah@qwinixtech.com>
2015-08-20 17:11:03 +0530

    cash burst added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_cache_buster
 
   def after_sign_up_path_for(resource)
     categories_path
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     categories_path
+  end
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = 1.years.ago.to_s
   end
 end

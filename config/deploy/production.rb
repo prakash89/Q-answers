d5813065de1319ad52b9ef4f4b3fd3f8a3e33ecb
Prakash <plingaiah@qwinixtech.com>
2015-08-21 16:57:20 +0530

    changes made

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,35 @@
-set :stage, :production
-set :branch, :master
-set :rails_env, :production
-set :deploy_to, '/u01/apps/qwinix/q_answers'
-set :log_level, :debug
+# set :stage, :production
+# set :branch, :master
+# set :rails_env, :production
+# set :deploy_to, '/u01/apps/qwinix/q_answers'
+# set :log_level, :debug
 
+# role :app, %w{deploy@54.191.65.34}
+# role :web, %w{deploy@54.191.65.34}
+# role :db,  %w{deploy@54.191.65.34}
+
+# # Extended Server Syntax
+# # ======================
+# # This can be used to drop a more detailed server
+# # definition into the server list. The second argument
+# # something that quacks like a hash can be used to set
+# # extended properties on the server.
+# server '54.191.65.34', :user => 'deploy', :roles => %w{web app db}
+# set :ssh_options, {
+#    #verbose: :debug,
+#    keys: %w(~/.ssh/id_rsa.pub),
+#    auth_methods: %w(publickey)
+# }
+
+set :branch, 'master'
+set :rails_env, 'production'
+set :deploy_to, '/u01/apps/qwinix/q_answers'
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
 role :app, %w{deploy@54.191.65.34}
 role :web, %w{deploy@54.191.65.34}
 role :db,  %w{deploy@54.191.65.34}
@@ -14,9 +40,29 @@ role :db,  %w{deploy@54.191.65.34}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '54.191.65.34', :user => 'deploy', :roles => %w{web app db}
-set :ssh_options, {
-   #verbose: :debug,
-   keys: %w(~/.ssh/id_rsa.pub),
-   auth_methods: %w(publickey)
-}
+server '54.191.65.34', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
+# set :unicorn_rack_env, 'production'
+#setting up development branch for dev environment
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
